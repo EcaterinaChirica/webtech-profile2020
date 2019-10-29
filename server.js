1ad9c1690f71528cf6b054a62eaddcc1d228e092
EC2 Default User <ec2-user@ip-172-31-90-230.ec2.internal>
2019-10-29 11:33:14 +0000

    sem 3

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+
+app.use('/', express.static('frontend'))
  
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/messages', function(request, response)
+{
+    var messages = []
+    var message = {
+        name: "Ecaterina",
+        message: "Hello from the web"
+        
+    }
+    messages.push(message)
+    response.status(200).send(messages);
 })
  
 app.listen(8080)
